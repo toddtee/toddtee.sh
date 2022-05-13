bc551caa786b1fbb31f8b0beed4050b86bf688c9
Todd Turner <todd.turner@fairfaxmedia.com.au>
2022-05-13 23:06:18 +1000

    :wrench: Production Infra Stack

---
+++ infrastructure/production/aws.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type AwsStackProps struct {
+	awscdk.StackProps
+}
+
+func NewAwsStack(scope constructs.Construct, id string, props *AwsStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+
+	bucketID := jsii.String("www.toddtee.sh")
+
+	bucket := awss3.NewBucket(stack, bucketID, &awss3.BucketProps{
+		BucketName:           bucketID,
+		PublicReadAccess:     jsii.Bool(true),
+		Versioned:            jsii.Bool(false),
+		WebsiteIndexDocument: jsii.String("index.html"),
+	})
+
+	UNUSED(bucket)
+
+	return stack
+}
+
+func UNUSED(x ...interface{}) {}
+
+func main() {
+	app := awscdk.NewApp(nil)
+
+	NewAwsStack(app, *jsii.String("static-toddtee-production"), &AwsStackProps{
+		awscdk.StackProps{
+			Synthesizer: awscdk.NewDefaultStackSynthesizer(&awscdk.DefaultStackSynthesizerProps{
+				FileAssetsBucketName: jsii.String("teamturner-aws-cdk"),
+			}),
+			Env: env(),
+		},
+	})
+
+	app.Synth(nil)
+
+}
+
+// env determines the AWS environment (account+region) in which our stack is to be deployed.
+func env() *awscdk.Environment {
+	return &awscdk.Environment{
+		Account: awscdk.Aws_ACCOUNT_ID(),
+		Region:  jsii.String("ap-southeast-2"),
+	}
+}
---
+++ infrastructure/production/aws_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/dev/dev-cdk-go/awscdk/v2"
+// 	assertions "github.com/dev/dev-cdk-go/awscdk/v2/assertions"
+// 	"github.com/dev/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in aws_test.go
+// func TestAwsStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewAwsStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
